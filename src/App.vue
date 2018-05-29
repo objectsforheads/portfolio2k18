8bf37e83bc15a1954a057d62067c3bd18419bd21
objectsforheads <dev@objectsforheads.com>
2018-05-29 00:16:24 -0700

    add fonts

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,11 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '~@/styles/fonts.scss';
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'renner', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
